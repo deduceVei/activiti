f054f618cb58b8d91157d44d28ca0c557ef48a73
deduce <462707165@qq.com>
2020-09-12 11:49:30 +0800

    no message

--- activi/designModel/StrategyMethod/src/IFlyBehavior.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author deduce
- */
-public interface IFlyBehavior {
-	/**
-	 * 此公共行为接口的被实现方法
-	 */
-	void fly();
-}
--- activi/designModel/StrategyMethod/src/WingsFlyBehavior.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author deduce
- */
-public class WingsFlyBehavior implements IFlyBehavior {
-	@Override
-	public void fly() {
-		System.out.println("WingsFlyBehavior...");
-	}
-}
